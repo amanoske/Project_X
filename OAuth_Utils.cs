dd29e6c333cb2a49d08f293a4eb91facaa401fa0
Andrew "Andy" Manoske <andrew.manoske@gmail.com>
2012-08-29 15:58:36 -0700

    The actual code
    Fucking magnets. How do they work?

---
+++ OAuth_Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Linq;
+using System.Xml.Linq;
+using System.Data.Linq;
+
+namespace Moped_WP7_Tester
+{
+    public class OAuth_Utils
+    {
+        string ConsumerKey;
+        string ConsumerSecret;
+
+        public OAuth_Utils(string c_key, string c_secret)
+        {
+            ConsumerKey = c_key;
+            ConsumerSecret = c_secret;
+        }
+        public void GetPIN()
+        {
+           
+        }
+        
+        
+    }
+}
